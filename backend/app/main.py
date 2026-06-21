c76c6a8ac007482fb1ba3f6dc35804f624c0b298
vishal-chaudhary23 <vishalkmrchaudhary@gmail.com>
2026-06-21 15:35:23 +0530

    Add Vercel domain to CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://enterprise-rag-platform-1mfcjdlel-code-cretaor.vercel.app"
+        ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
